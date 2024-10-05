70370882d031a1b9b355b1a8756a9c520ff49f47
wiktq <wiktqblog@gmail.com>
2024-10-05 13:19:17 +0200

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -10,6 +10,6 @@ RESUME_FILE = ROOT_DIR / "resume_state.json"
 RAW_DIR.mkdir(exist_ok=True)
 
 # Twitter credentials
-TWITTER_USERNAME = "my_twitter_username"
-TWITTER_EMAIL = "my_email"
-TWITTER_PASSWORD = "my_password"
+TWITTER_USERNAME = "Marta1221502"
+TWITTER_EMAIL = "wiktorynka_0808@wp.pl"
+TWITTER_PASSWORD = "Twitterpassword"
