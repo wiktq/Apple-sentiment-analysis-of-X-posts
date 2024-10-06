c872813c0d53c1c5d5ac9956aa1fb13bf9bee689
wiktq <wiktqblog@gmail.com>
2024-10-06 17:38:47 +0200

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -24,12 +24,12 @@ MINIMUM_TWEETS = 500  # Adjust based on your goal of 20k-30k total tweets
 
 # List of dates with 7-day range before and after
 DATES = [
-    ("2011-08-22", "2011-08-24", "aug_24_2011.csv", ["Apple"]),
-    ("2010-01-26", "2010-01-28", "jan_27_2010.csv", ["Apple"]),
-    ("2014-08-28", "2014-08-30", "aug_31_2014.csv", ["Apple"]),
-    ("2010-03-03", "2010-03-05", "mar_5_2010.csv", ["Apple"]),
-    ("2011-08-03", "2011-08-05", "aug_3_2011.csv", ["Apple"]),
-    ("2014-05-11", "2014-05-13", "may_12_2014.csv", ["Apple"]),
+    ("2011-08-23", "2011-08-25", "aug_24_2011.csv", ["Apple"]),
+    ("2010-01-27", "2010-01-29", "jan_27_2010.csv", ["Apple"]),
+    ("2014-08-29", "2014-08-31", "aug_31_2014.csv", ["Apple"]),
+    ("2010-03-04", "2010-03-06", "mar_5_2010.csv", ["Apple"]),
+    ("2011-08-04", "2011-08-06", "aug_3_2011.csv", ["Apple"]),
+    ("2014-05-12", "2014-05-14", "may_12_2014.csv", ["Apple"]),
 ]
 
 async def get_tweets(client: Client, query: str, tweets: Optional[object]) -> object:
