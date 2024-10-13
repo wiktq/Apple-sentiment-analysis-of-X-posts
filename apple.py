ffc94f6c26ced3e380f521fbe12f126ef767a216
wiktq <wiktqblog@gmail.com>
2024-10-13 17:46:00 +0200

    Update apple.py

--- apple.py
+++ apple.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Load your CSV file into a DataFrame
-csv_file = "/Users/wiktoria/Documents/master/Master thesis/May 12, 2014/May_12_2014_merged_links.csv"  # Replace with your file path
+csv_file = "/Users/wiktoria/Documents/master/Master thesis/All tweets/tweets_merged_links.csv"  # Replace with your file path
 df = pd.read_csv(csv_file)
 
 # Filter rows where 'apple' is present in the tweet (case-insensitive)
