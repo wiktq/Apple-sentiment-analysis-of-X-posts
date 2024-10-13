b81fe9d9bbfc6f9994e10d645eb7fa37cf856aad
wiktq <wiktqblog@gmail.com>
2024-10-13 19:01:36 +0200

    Create lowercase.py

---
+++ lowercase.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+# Load your CSV file with tweets
+df = pd.read_csv('/Users/wiktoria/Documents/master/Master thesis/August 24, 2011 - Steve Jobs resignation/August_24_2011_merged_links+apple+query+nick+dup+ASCII.csv')
+
+# Convert the 'Text' column (or your equivalent tweet text column) to lowercase
+df['Text'] = df['Text'].str.lower()
+
+# Save the modified dataframe to a new CSV file
+df.to_csv('lowercase_tweets.csv', index=False)
+
+print("All tweets have been converted to lowercase.")
