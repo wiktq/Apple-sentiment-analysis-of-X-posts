d5da979f73c01687ef2a4250135eb87ab9b2764b
wiktq <wiktqblog@gmail.com>
2024-10-13 18:30:54 +0200

    Update duplicate.py

--- duplicate.py
+++ duplicate.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Load the CSV file into a DataFrame
-df = pd.read_csv("/Users/wiktoria/Documents/master/Master thesis/May 12, 2014/May_12_2014_merged_links+apple+query+nick.csv")
+df = pd.read_csv("/Users/wiktoria/Documents/master/Master thesis/All tweets/tweets_merged_links+apple+query+nick.csv")
 
 # Drop duplicate tweets (keep the first occurrence)
 df_cleaned = df.drop_duplicates(subset=['Text'], keep='first')
