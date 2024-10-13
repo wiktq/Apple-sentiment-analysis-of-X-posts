30112f28fc9b899440e5b3604cd84923f7fafd32
wiktq <wiktqblog@gmail.com>
2024-10-13 17:58:42 +0200

    Update query.py

--- query.py
+++ query.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Define the file path
-csv_file = "/Users/wiktoria/Documents/master/Master thesis/August 31, 2014 - iCloud leak/August_24_2014_merged_links+apple.csv"  # Replace with your file path
+csv_file = "/Users/wiktoria/Documents/master/Master thesis/March 5, 2010/March_5_2010_merged_links+apple.csv"  # Replace with your file path
 
 # Load your CSV file into a DataFrame
 df = pd.read_csv(csv_file)
