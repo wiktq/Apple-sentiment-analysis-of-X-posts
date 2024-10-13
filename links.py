d78eb77dd8a20c1dad4f7fc5e4e2b149eadb90b9
wiktq <wiktqblog@gmail.com>
2024-10-13 17:28:23 +0200

    Update links.py

--- links.py
+++ links.py
@@ -2,7 +2,7 @@ import pandas as pd
 import re
 
 # Load your CSV file into a DataFrame
-csv_file = "/Users/wiktoria/Documents/master/Master thesis/August 24, 2011 - Steve Jobs resignation/August_24_2011_merged copy.csv"  # Replace with your file path
+csv_file = "/Users/wiktoria/Documents/master/Master thesis/January 27, 2010 - iPad announcement/January_27_2010_merged.csv"  # Replace with your file path
 df = pd.read_csv(csv_file)
 
 # Function to remove URLs
