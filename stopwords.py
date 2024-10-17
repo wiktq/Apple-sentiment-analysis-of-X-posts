cb9fdfb5ff4e198050a8e2acc57ffb2198b04abb
wiktq <wiktqblog@gmail.com>
2024-10-17 13:10:14 +0200

    Update stopwords.py

--- stopwords.py
+++ stopwords.py
@@ -6,7 +6,7 @@ from nltk.corpus import stopwords
 nltk.download('stopwords')
 
 # Load your CSV file
-csv_file = '/Users/wiktoria/Documents/master/Master thesis/August 31, 2014 - iCloud leak/August_24_2014_merged_links+apple+query+nick+dup+ASCII+lowercase.csv'  # Change to your actual file path
+csv_file = '/Users/wiktoria/Documents/master/Master thesis/March 5, 2010/March_5_2010_merged_links+apple+query+nick+dup+ASCII+lowercase.csv'  # Change to your actual file path
 df = pd.read_csv(csv_file)
 
 # Define stop words
